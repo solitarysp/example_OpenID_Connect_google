5f2de3a93db9183a0a7e251a3e17e196860cb7ef
lethanh98 <lethanh9398@gmail.com>
2020-05-18 23:50:46 +0700

    d

--- src/main/java/lethanh98/controller/LoginGoogleController.java
+++ src/main/java/lethanh98/controller/LoginGoogleController.java
@@ -4,10 +4,6 @@ import lethanh98.config.teamplate.RestAll;
 import lethanh98.dto.request.LoginGoogleRequestDto;
 import lethanh98.dto.response.LoginGoogleResponseDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,27 +19,37 @@ public class LoginGoogleController {
     RestAll restAll;
 
     @GetMapping("/getInfoUser")
-    public LoginGoogleResponseDto get(@RequestParam("code") String code) {
-        LoginGoogleRequestDto checkRequestDto = LoginGoogleRequestDto.builder()
-                .code(code)
-                .clientId("601662646531-aaja6d30rsh2b6mt4nm7f2dovnm7pup0.apps.googleusercontent.com")
-                .clientSecret("8oWBL4fnEYX4_sspOvct9YW0")
-                .redirectUri("http://localhost:8080/OIDC/getInfoUser")
-                .grantType("authorization_code")
-                .build();
-        HttpEntity<LoginGoogleRequestDto> requestEntityRemit = new HttpEntity<>(checkRequestDto);
+    public LoginGoogleResponseDto get(@RequestParam(value = "token") String token) {
 
-        ResponseEntity<LoginGoogleResponseDto> response = restAll.restTemplate().exchange("https://oauth2.googleapis.com/token", HttpMethod.POST, requestEntityRemit, new ParameterizedTypeReference<LoginGoogleResponseDto>() {
-        });
-        LoginGoogleResponseDto loginGoogleResponseDto = response.getBody();
+
+        LoginGoogleResponseDto loginGoogleResponseDto = new LoginGoogleResponseDto();
+        loginGoogleResponseDto.setAccessToken(token);
         loginGoogleResponseDto.getUrl().put("Lấy thông tin user", "https://openidconnect.googleapis.com/v1/userinfo?access_token=" + loginGoogleResponseDto.getAccessToken());
         return loginGoogleResponseDto;
+    }
+
+    @GetMapping("/getToken")
+    public String getToken() {
+        return "<!DOCTYPE html>\n" +
+                "<html>\n" +
+                "<body>\n" +
+                "\n" +
+                "<h1>My First Heading</h1>\n" +
+                "<p>My first paragraph.</p>\n" +
+                "\n" +
+                "</body>\n" +
+                "<script>\n" +
+                "var token = window.location.hash.split('#access_token=')[1];\n" +
+                "window.location.href = \"http://\"+window.location.host +'/OIDC/getInfoUser?token='+token;\n" +
+                "\n" +
+                "</script> "+
+                "</html>";
     }
 
     @GetMapping("Info")
     public Map<String, String> login() {
         Map<String, String> url = new HashMap<>();
-        url.put("Authentication google", "https://accounts.google.com/o/oauth2/v2/auth?client_id=601662646531-aaja6d30rsh2b6mt4nm7f2dovnm7pup0.apps.googleusercontent.com&scope=openid%20email%20profile&redirect_uri=http://localhost:8080/OIDC/getInfoUser&response_type=code&prompt=consent&access_type=offline");
+        url.put("Authentication google", "https://accounts.google.com/o/oauth2/v2/auth?client_id=601662646531-aaja6d30rsh2b6mt4nm7f2dovnm7pup0.apps.googleusercontent.com&scope=openid%20email%20profile&redirect_uri=http://localhost:8080/OIDC/getToken&response_type=token&prompt=consent");
         url.put("openid-configuration", "https://accounts.google.com/.well-known/openid-configuration");
         return url;
     }
